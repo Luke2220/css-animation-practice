c158fb315de15be11efeaad1726f2cb841e77585
Luke <kingluke2222@live.com>
2021-09-05 11:54:11 -0700

    animate sliding dropdown

--- drop-down.js
+++ drop-down.js
@@ -3,6 +3,7 @@ const dropdown = document.getElementsByClassName('dropdown')[0];
 
 function toggleVisible(element){
     element.classList.toggle('visible');
+    element.classList.toggle('transition-slide');
 }
 
 dropdownBox.addEventListener('click', ( )=> {
